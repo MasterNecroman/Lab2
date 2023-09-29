c23e80b53f1562ed1a1bfc3207ee7707501c3b91
MasterNecroman <138460288+MasterNecroman@users.noreply.github.com>
2023-09-29 18:19:30 +0300

    Update Program.cs

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -13,67 +13,50 @@ namespace Task_2
 
         static void MainLoop(Converter converter)
         {
-            while (true)
+            double amount = GetAmount();
+            Currency sourceCurrency = GetCurrency("Enter the currency to convert (USD, EUR, PLN):");
+            Currency targetCurrency = GetCurrency("Enter the target currency (USD, EUR, PLN):");
+
+            double result = converter.Convert(amount, sourceCurrency, targetCurrency);
+            Console.WriteLine($"Conversion result: {result} {targetCurrency}");
+
+            Console.WriteLine("Do you want to convert another amount? (yes/no)");
+            string continueStr = Console.ReadLine()?.ToLower();
+
+            if (continueStr == "yes")
             {
-                double amount = GetAmount();
-                Currency sourceCurrency = GetCurrency("Enter the currency to convert (USD, EUR, PLN):");
-                Currency targetCurrency = GetCurrency("Enter the target currency (USD, EUR, PLN):");
-
-                double result = converter.Convert(amount, sourceCurrency, targetCurrency);
-                Console.WriteLine($"Conversion result: {result} {targetCurrency}");
-
-                Console.WriteLine("Do you want to convert another amount? (yes/no)");
-                string continueStr = Console.ReadLine()?.ToLower();
-
-                if (continueStr != "yes")
-                {
-                    break;
-                }
+                MainLoop(converter);
+            }
+            else
+            {
+                Console.WriteLine("Exiting the program.");
             }
         }
 
         static double GetAmount()
         {
-            double amount;
+            Console.WriteLine("Enter the amount:");
+            string amountStr = Console.ReadLine();
 
-            while (true)
+            if (double.TryParse(amountStr, out double amount))
             {
-                Console.WriteLine("Enter the amount:");
-                string amountStr = Console.ReadLine();
-
-                if (string.IsNullOrEmpty(amountStr) || !double.TryParse(amountStr, out amount))
-                {
-                    Console.WriteLine("Invalid amount. Please try again.");
-                }
-                else
-                {
-                    break;
-                }
+                return amount;
             }
-
-            return amount;
+            Console.WriteLine("Invalid amount. Please try again.");
+            return GetAmount();
         }
 
-        static Currency GetCurrency(string message)
+        static Currency GetCurrency(string prompt)
         {
-            Currency currency;
+            Console.WriteLine(prompt);
+            string currencyStr = Console.ReadLine();
 
-            while (true)
+            if (Enum.TryParse(currencyStr, out Currency currency) && Enum.IsDefined(typeof(Currency), currency))
             {
-                Console.WriteLine(message);
-                string currencyStr = Console.ReadLine();
-
-                if (Enum.TryParse(currencyStr, out currency) && Enum.IsDefined(typeof(Currency), currency))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid currency. Please try again.");
-                }
+                return currency;
             }
-
-            return currency;
+            Console.WriteLine("Invalid currency. Please try again.");
+            return GetCurrency(prompt);
         }
     }
 }
